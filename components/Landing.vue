bd3c400219a9d5aa201bf903c1c1b16b8228a152
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2017-10-03 21:30:32 +0200

    Primeros componentes

---
+++ components/Landing.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="landing">
+    <h2>Pills</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'landing'
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/_colors';
+
+.landing {
+  display: flex;
+  height: 70vh;
+
+  background: linear-gradient(#42B883 0%, #35495E 100%);
+
+  justify-content: center;
+  align-items: center;
+
+  h2 {
+    font-size: 5rem;
+    color: $color-basic-ligth;
+  }
+}
+</style>
+
+
---
+++ components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="navbar">
+    <h1>Pills</h1>
+
+    <nav>
+      <ul>
+        <li>Acerca de</li>
+        <li>Partreon</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/_colors';
+
+.navbar {
+  position: fixed;
+  background: $color-basic-ligth;
+  left: 0;
+  right: 0;
+  height: 5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+  h1 {
+    margin: 0;
+  }
+
+  ul {
+    list-style: none;
+    background: red;
+    margin: 0;
+    padding: 0;
+
+    li {
+      display: inline-block;
+    }
+  }
+}
+</style>
+
+
+
