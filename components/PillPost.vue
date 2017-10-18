d76a7356545a596fe6d398c1a5292c34f85c4332
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2017-10-18 21:18:00 +0200

    [#15] Componente PillPost

---
+++ components/PillPost.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="pill-post">
+    <app-subtitle>Resumen</app-subtitle>
+
+    <article>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo voluptatum omnis repellendus in atque dolores veniam, veritatis corporis aperiam suscipit sint sequi natus molestias soluta nobis cumque labore exercitationem?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo voluptatum omnis repellendus in atque dolores veniam, veritatis corporis aperiam suscipit sint sequi natus molestias soluta nobis cumque labore exercitationem?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo voluptatum omnis repellendus in atque dolores veniam, veritatis corporis aperiam suscipit sint sequi natus molestias soluta nobis cumque labore exercitationem?
+      </p>
+    </article>
+  </section>
+</template>
+
+<script>
+import AppSubtitle from '~/components/AppSubtitle';
+
+export default {
+  name: 'pill-post',
+  components: {
+    AppSubtitle
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '/../assets/scss/_extends';
+
+.pill-post {
+  @extend .container;
+}
+</style>
+
