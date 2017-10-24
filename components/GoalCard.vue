6f827cb32daa5e726e1c29d2169da2d1114304da
José Antonio Dongil <joseantonio.dongil@bbva.com>
2017-10-24 13:28:31 +0200

    [#20] Component GoalCard

---
+++ components/GoalCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="goal-card">
+    <img :src="goal.img" :alt="goal.title">
+    <h3>{{ goal.title }}</h3>
+    <p>{{ goal.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goal-card',
+  props: {
+    goal: { type: Object, required: true }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.goal-card {
+  img {
+    width: 100%;
+  }
+}
+</style>
+
+
--- pages/_lang/about.vue
+++ pages/_lang/about.vue
@@ -1,22 +1,56 @@
 <template>
-  <app-title>
-    {{ $t('default.about') }}
-  </app-title>
+  <main>
+    <app-title>
+      {{ $t('default.about') }}
+    </app-title>
+
+    <goal-section>
+      <goal-card v-for="goal in goals" :key="goal.id" :goal="goal" />
+    </goal-section>
+  </main>
 </template>
 
 <script>
 import AppTitle from '~/components/AppTitle';
+import GoalSection from '~/components/GoalSection';
+import GoalCard from '~/components/GoalCard';
 
 export default {
   name: 'about',
-  head () {
+  head() {
     return {
       title: this.$t('default.about')
     }
   },
   components: {
-    AppTitle
+    AppTitle,
+    GoalSection,
+    GoalCard
+  },
+  data() {
+    return {
+      goals: [{
+        id: 1,
+        img: 'http://lorempixel.com/400/200',
+        title: 'Aprende a tu ritmo',
+        description: 'Pills está pensado para gente que cuenta con poco tiempo. Aprovecha esos minutos que te sobran para aprender algo nuevo cada día',
+        ready: true
+      },
+      {
+        id: 2,
+        img: 'http://lorempixel.com/400/200',
+        title: 'Gestiona tu progreso',
+        description: 'Con Pills, es más fácil saber cuándo y cómo estuiar. Planifica tus sesiones de formación y decide cuáles serán tus objetivos',
+        ready: false
+      },
+      {
+        id: 3,
+        img: 'http://lorempixel.com/400/200',
+        title: 'Comparte tu conocimiento',
+        description: 'Crea Pills para que otros puedan disfrutar de lo que sabes. De esta forma, podrás ayudar a crear una mejor comunidad',
+        ready: false
+      }]
+    }
   }
 }
 </script>
-
