272c6b9080b50587912df6ad8e9c295f65b9f0ea
José Antonio Dongil <joseantonio.dongil@bbva.com>
2017-10-25 10:30:08 +0200

    [#20] Component GoalCard

--- components/GoalCard.vue
+++ components/GoalCard.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="goal-card">
+    <span class="goal-card-ready" v-if="!goal.ready">En desarrollo</span>
     <img :src="goal.img" :alt="goal.title">
     <h3>{{ goal.title }}</h3>
     <p>{{ goal.description }}</p>
@@ -16,7 +17,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.goal-card-ready {
+  background: yellow;
+  display: inline-block;
+  padding: 0.1rem 0.5rem;
+  position: absolute;
+  top: 1rem;
+  margin-left: -0.5rem;
+}
+
 .goal-card {
+  position: relative;
+
   img {
     width: 100%;
     border-radius: 2px;
